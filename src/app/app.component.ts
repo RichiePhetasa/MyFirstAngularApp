082985e8d07fd8b5e37de98d815ff22f04f498d8
RichiePhetasa <Richiep559@mail.fresnostate.edu>
2019-03-11 21:58:24 -0700

    add bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'MyFirstAngularApp';
+  contacts = [];
+  constructor () {
+    this.contacts = [
+      {
+        ID: 1,
+        first_name: 'Richie',
+        last_name: 'Phetasa',
+        email: 'Richiep559@mail.fresnostate.edu'
+      },
+      {
+        ID: 2,
+        first_name: 'Joe',
+        last_name: 'Biden',
+        email: 'Richiep559@mail.fresnostate.edu'
+      },
+      {
+        ID: 3,
+        first_name: 'Mike',
+        last_name: 'Tyson',
+        email: 'Richiep559@mail.fresnostate.edu'
+      }
+    ]
+  }
+  delete(e) {
+    console.log('from delete e: ' , e);
+    this.contacts.splice(e, 1);
+  }
+  addRow() {
+    this.contacts.unshift({
+
+    });
+  }
+  save(row) {
+    console.log('from save row:', row);
+  }
 }
